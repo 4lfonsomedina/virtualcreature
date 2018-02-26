7950a4c0c4578914f279501e42f414a84686cca5
Alfonso Medina Duran <4lfonsomedina@gmail.com>
2018-02-26 14:42:22 -0800

    bienvenida

---
+++ index.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="css/bootstrap.css">
+<script src="js/jquerymin.js"></script>
+<link rel="stylesheet" href="css/bootstrap.js">
+
+<div class="container">
+	<div class="jumbotron">
+	  <h1 class="display-4">Hello! Welcome to VirtualCreature.com</h1>
+	  <p class="lead">Coming soon: 04/04/2018.</p>
+	</div>
+	<div class="row" style="text-align: center">
+
+		<div class="col-md-2">
+			<img src="img/creature1.gif">
+		</div>
+		<div class="col-md-2">
+			<img src="img/creature4.gif">
+		</div>
+		<div class="col-md-2">
+			<img src="img/creature2.gif">
+		</div>
+		<div class="col-md-2">
+			<img src="img/creature6.gif">
+		</div>
+		<div class="col-md-2">
+			<img src="img/creature3.gif">
+		</div>
+		<div class="col-md-2">
+			<img src="img/creature7.gif">
+		</div>	
+
+	</div>
+
+</div>
