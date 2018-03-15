32be30542877d6bf5411df0ba4063f977eb85116
4lfonsomedina <4lfonsomedina@gmail.com>
2018-03-14 20:36:18 -0700

    ya se vacuna

--- js/virtualcreature.js
+++ js/virtualcreature.js
@@ -1,58 +1,66 @@
 $(document).ready(function(){
-	var t_caminar;
-	estado_normal_iniciar();
-//tiempo
-	function estado_normal_iniciar(){
-		clearInterval(t_caminar);
-		t_caminar = setInterval(function(){ caminar(); }, 8000);
-	}
-	function estado_normal_parar(){
-		clearInterval(t_caminar);
-	}
-	
-//caminar
-	function caminar(){
-		var dir 		= Math.floor((Math.random() * 2)+1);
-		var imagen 		= "";
-		var imagenE 	= "";
-		var posicion 	= "";
-		posicion = get_posicion();
-		imagen 		= $("#vc_c"+posicion).html();
-		imagenE 	= $("#vc_ce"+posicion).html();
-		$("#vc_ce"+posicion).html("");
-		$("#vc_c"+posicion).html("");
-		if(posicion==1){dir=2;}
-		if(posicion==20){dir=1;}
-		if(dir==1){posicion--;}else{posicion++;}
-		$("#vc_c"+posicion).html(imagen);
-		$("#vc_ce"+posicion).html(imagenE);
+///////////////////////////////////////////////////////////////////////	
+//******************************** TIEMPO **************************//
+/////////////////////////////////////////////////////////////////////
+var t_caminar;
+estado_normal_iniciar();
+function estado_normal_iniciar(){
+	clearInterval(t_caminar);
+	t_caminar = setInterval(function(){ caminar(); }, 8000);
+}
+function estado_normal_parar(){
+	clearInterval(t_caminar);
+}
+///////////////////////////////////////////////////////////////////////	
+//******************************** COMIDA **************************//
+/////////////////////////////////////////////////////////////////////
+function caminar(){
+	var dir 		= Math.floor((Math.random() * 2)+1);
+	var imagen 		= "";
+	var imagenE 	= "";
+	var posicion 	= "";
+	posicion = get_posicion();
+	imagen 		= $("#vc_c"+posicion).html();
+	imagenE 	= $("#vc_ce"+posicion).html();
+	$("#vc_ce"+posicion).html("");
+	$("#vc_c"+posicion).html("");
+	if(posicion==1){dir=2;}
+	if(posicion==20){dir=1;}
+	if(dir==1){posicion--;}else{posicion++;}
+	$("#vc_c"+posicion).html(imagen);
+	$("#vc_ce"+posicion).html(imagenE);
 	}//caminar
+
+///////////////////////////////////////////////////////////////////////	
+//******************************* ALEGRIA **************************//
+/////////////////////////////////////////////////////////////////////
 //alegria
-	$(".vc_celda").click(function(){
-		if($(this).html().indexOf("creature")>=0&&ocupado()){
-			alegria();
-		}
-	})
-	function alegria(){
-		var posicion = get_posicion();
-		var lvl 	= "lvl1";
-		var alegria = "<img src='img/creature/"+lvl+"/creature6.gif'>";
-		var estado 	= "<img src='img/creature/estatus/estatus2.gif'>";
-		var criatura_actual  = $("#vc_c"+posicion).html();
-		var estado_actual  = $("#vc_ce"+posicion).html();
-		estado_normal_parar();
-		$("#vc_c"+posicion).html(alegria);
-		$("#vc_ce"+posicion).html(estado);
-		setTimeout(function(){
-			$("#vc_c"+posicion).html(criatura_actual);
-			$("#vc_ce"+posicion).html(estado_actual);
-			estado_normal_iniciar();
-		},4000);
+$(".vc_celda").click(function(){
+	if($(this).html().indexOf("creature")>=0&&ocupado()){
+		alegria();
+	}
+})
+function alegria(){
+	var posicion = get_posicion();
+	var lvl 	= "lvl1";
+	var alegria = "<img src='img/creature/"+lvl+"/creature6.gif'>";
+	var estado 	= "<img src='img/creature/estatus/estatus2.gif'>";
+	var criatura_actual  = $("#vc_c"+posicion).html();
+	var estado_actual  = $("#vc_ce"+posicion).html();
+	estado_normal_parar();
+	$("#vc_c"+posicion).html(alegria);
+	$("#vc_ce"+posicion).html(estado);
+	setTimeout(function(){
+		$("#vc_c"+posicion).html(criatura_actual);
+		$("#vc_ce"+posicion).html(estado_actual);
+		estado_normal_iniciar();
+	},4000);
 
 
-	}//alegria
-
-//comer
+}
+///////////////////////////////////////////////////////////////////////	
+//******************************** COMER ***************************//
+/////////////////////////////////////////////////////////////////////
 $("#btn_m_comer").click(function(){
 	if(ocupado()){
 		comer();
@@ -69,15 +77,16 @@ function comer(){
 	$("#vc_c"+(posicion+1)).html(ima_cominda);
 	$("#vc_c"+posicion).html("<img src='img/creature/"+lvl+"/creature4.gif'>");
 	setTimeout(function(){
-			$("#vc_c"+posicion).html(criatura_actual);
-			$("#vc_c"+(posicion+1)).html("");
-			$("#vc_ce"+posicion).html(estado_actual);
-			alegria();
-			estado_normal_iniciar();
-		},7500);
-}//comer
-
-//banio
+		$("#vc_c"+posicion).html(criatura_actual);
+		$("#vc_c"+(posicion+1)).html("");
+		$("#vc_ce"+posicion).html(estado_actual);
+		alegria();
+		estado_normal_iniciar();
+	},7500);
+}
+///////////////////////////////////////////////////////////////////////	
+//******************************** BANIO ***************************//
+/////////////////////////////////////////////////////////////////////
 $("#btn_m_limpiar").click(function(){
 	if(ocupado()){
 		banio();
@@ -94,16 +103,68 @@ function banio(){
 	$("#vc_ce"+(posicion+1)).html(ima_banio);
 	$("#vc_c"+posicion).html("<img src='img/creature/"+lvl+"/creature1.gif'>");
 	setTimeout(function(){
-			$("#vc_c"+posicion).html(criatura_actual);
-			$("#vc_ce"+(posicion+1)).html("");
+		$("#vc_c"+posicion).html(criatura_actual);
+		$("#vc_ce"+(posicion+1)).html("");
 			$("#vc_ce"+posicion).html(estado_actual);// aqui hay que quitar las moscas en caso de tenerlas
 			alegria();
 			estado_normal_iniciar();
 		},7500);
-}//comer
-//banio
-
-//funcion de ocupacion es para verificar que no este ocupado al querer ejecutar otra accion
+}
+///////////////////////////////////////////////////////////////////////	
+//******************************* VACUNA ***************************//
+/////////////////////////////////////////////////////////////////////
+$("#btn_m_vacuna").click(function(){
+	if(ocupado()){
+		vacuna();
+	}
+})
+function vacuna(){
+	var posicion = get_posicion();
+	var lvl = "lvl1";
+	var ima_vacuna = "<img src='img/acciones/vacuna.gif'>";
+	estado_normal_parar();
+	var criatura_actual  = $("#vc_c"+posicion).html();
+	var estado_actual  = $("#vc_ce"+posicion).html();
+	if(posicion==20){posicion=posicion-1;}
+	$("#vc_ce"+(posicion+1)).html(ima_vacuna);
+	$("#vc_c"+posicion).html("<img src='img/creature/"+lvl+"/creature3.gif'>");
+	setTimeout(function(){
+		$("#vc_c"+posicion).html(criatura_actual);
+		$("#vc_ce"+(posicion+1)).html("");
+			$("#vc_ce"+posicion).html(estado_actual);// aqui hay que quitar las calaca en caso de tenerla
+			alegria();
+			estado_normal_iniciar();
+		},7500);
+}
+///////////////////////////////////////////////////////////////////////	
+//******************************* VACUNA ***************************//
+/////////////////////////////////////////////////////////////////////
+$("#btn_m_vacuna").click(function(){
+	if(ocupado()){
+		vacuna();
+	}
+})
+function vacuna(){
+	var posicion = get_posicion();
+	var lvl = "lvl1";
+	var ima_vacuna = "<img src='img/acciones/vacuna.gif'>";
+	estado_normal_parar();
+	var criatura_actual  = $("#vc_c"+posicion).html();
+	var estado_actual  = $("#vc_ce"+posicion).html();
+	if(posicion==20){posicion=posicion-1;}
+	$("#vc_ce"+(posicion+1)).html(ima_vacuna);
+	$("#vc_c"+posicion).html("<img src='img/creature/"+lvl+"/creature3.gif'>");
+	setTimeout(function(){
+		$("#vc_c"+posicion).html(criatura_actual);
+		$("#vc_ce"+(posicion+1)).html("");
+			$("#vc_ce"+posicion).html(estado_actual);// aqui hay que quitar las calaca en caso de tenerla
+			alegria();
+			estado_normal_iniciar();
+		},7500);
+}
+///////////////////////////////////////////////////////////////////////	
+//****************************** OCUPADO? **************************//
+/////////////////////////////////////////////////////////////////////
 function ocupado(){
 	var ocu = false;
 	$(".vc_celda").each(function(index){
@@ -113,11 +174,13 @@ function ocupado(){
 			$("#vc_c"+(posicion+1)).html()==""&&
 			$("#vc_ce"+(posicion+1)).html()==""){
 			ocu=true;
-		}
-	})
+	}
+})
 	return ocu;
 }
-//regresa la pocicion de la creatura
+///////////////////////////////////////////////////////////////////////	
+//****************************** POSICION **************************//
+/////////////////////////////////////////////////////////////////////
 function get_posicion(){
 	var posicion = 0;
 	$(".vc_celda").each(function(index){
@@ -127,13 +190,17 @@ function get_posicion(){
 	})
 	return posicion;
 }
+///////////////////////////////////////////////////////////////////////	
+//******************************** MENU **************************//
+/////////////////////////////////////////////////////////////////////
+$( ".vc_menu a" ).hover(
+	function() {
+		$(this).find("img").attr("src","/img/menu/h_"+$(this).attr("btn")+".png");
+	}, function() {
+		$(this).find("img").attr("src","/img/menu/"+$(this).attr("btn")+".png");
+	}
+);
 
-//menu
-	$( ".vc_menu a" ).hover(
-	  function() {
-	  	$(this).find("img").attr("src","/img/menu/h_"+$(this).attr("btn")+".png");
-	  }, function() {
-	    $(this).find("img").attr("src","/img/menu/"+$(this).attr("btn")+".png");
-	  }
-	);//menu
+
+
 })
--- v1.php
+++ v1.php
@@ -46,7 +46,7 @@
 				<div class="row">
 					<div class="col-xs-3 vc_menu"><a href="#" id="btn_m_comer" btn="m_comer"><img src="img/menu/m_comer.png"></a></div>
 					<div class="col-xs-3 vc_menu"><a href="#" id="btn_m_limpiar" btn="m_limpiar"><img src="img/menu/m_limpiar.png"></a></div>
-					<div class="col-xs-3 vc_menu"><a href="#" btn="m_vacuna"><img src="img/menu/m_vacuna.png"></a></div>
+					<div class="col-xs-3 vc_menu"><a href="#" id="btn_m_vacuna" btn="m_vacuna"><img src="img/menu/m_vacuna.png"></a></div>
 					<div class="col-xs-3 vc_menu"><a href="#" btn="m_info"><img src="img/menu/m_info.png"></a></div>
 				</div>
 				<div class="row">
